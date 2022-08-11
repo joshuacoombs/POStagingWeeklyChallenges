eaaa7ccfa2dae4ee0549f9e3c530d8486bfdf796
joshuacoombs <106624478+joshuacoombs@users.noreply.github.com>
2022-08-11 08:48:50 -0700

    Update QuadraticEquationRoot.java

--- QuadraticEquationRoot.java
+++ QuadraticEquationRoot.java
@@ -1,6 +1,12 @@
 
 public class QuadraticEquationRoot {
-	public int quadraticEquation(int a, int b, int c) {
-		
+	public static int quadraticEquation(int a, int b, int c) {
+		return (int) Math.ceil(((-b/(2*a)) + Math.sqrt(Math.pow(b, 2) - (4*a*c))/(2*a)));
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(quadraticEquationRounded(1, 2, -3));
+		System.out.println(quadraticEquationRounded(2, -7, 3));
+		System.out.println(quadraticEquationRounded(1, -12, -28));
 	}
 }
